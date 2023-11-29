79c21d7dfad14800c869676fe8152bf70bf9541c
AxelCArette <axelcarette@gmail.com>
2023-11-29 14:41:12 +0100

    to do list

---
+++ ssccrriipptt.js
@@ -0,0 +1,7 @@
+let titre = document.querySelector(".titre")
+let son = document.querySelector(".son")
+console.log ("salut")
+titre.addEventListener("click",function(){
+    son.play()
+    console.log("Salut")
+})
